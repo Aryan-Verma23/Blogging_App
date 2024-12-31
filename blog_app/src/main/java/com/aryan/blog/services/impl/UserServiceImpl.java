d6f778d7ce972c76b504bfebe7def09a6778a80e
billlxclin <aryan9.verma09@gmail.com>
2024-12-31 20:52:25 +0530

    commit

--- blog_app/src/main/java/com/aryan/blog/services/impl/UserServiceImpl.java
+++ blog_app/src/main/java/com/aryan/blog/services/impl/UserServiceImpl.java
@@ -2,20 +2,33 @@ package com.aryan.blog.services.impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.aryan.blog.entities.User;
 import com.aryan.blog.payloads.UserDto;
+import com.aryan.blog.repositories.UserRepo;
 import com.aryan.blog.services.UserService;
 
 public class UserServiceImpl implements UserService {
+	
+	
+	@Autowired
+	private UserRepo userRepo;
 
 	@Override
-	public UserDto create(UserDto user) {
-		// TODO Auto-generated method stub
-		return null;
+	public UserDto create(UserDto userDto) {
+		User user = this.dtotoUser(userDto);
+		
+		User savedUser = this.userRepo.save(user);
+		
+		return this.usertoDto(savedUser);
 	}
 
 	@Override
 	public UserDto updateUser(UserDto user, int userId) {
-		// TODO Auto-generated method stub
+		
+		
+		
 		return null;
 	}
 
@@ -36,5 +49,29 @@ public class UserServiceImpl implements UserService {
 		// TODO Auto-generated method stub
 
 	}
+	
+	public User dtotoUser(UserDto userDto) {
+		User user=new User();
+		user.setId(userDto.getId());
+		user.setName(userDto.getName());
+		user.setEmail(userDto.getEmail());
+		user.setPassword(userDto.getPassword());
+		user.setAbout(userDto.getAbout());
+		return user;
+	}
+	public UserDto usertoDto(User user) {
+		UserDto userDto=new UserDto();
+		userDto.setId(user.getId());
+		userDto.setName(user.getName());
+		userDto.setEmail(user.getEmail());
+		userDto.setPassword(user.getPassword());
+		userDto.setAbout(user.getAbout());
+		
+		return userDto;
+		
+		
+	}
+	
+	
 
 }
